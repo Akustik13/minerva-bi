881f46113fbc3422df90dd2bfe03f69ae69b3790
Akustik13 <prym.via@gmail.com>
2026-05-12 09:33:38 +0200

    calendar_app: merge migration 0003 (resolve 0002 conflict)

---
+++ calendar_app/migrations/0003_merge.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('calendar_app', '0002_alter_calendarevent_email_message_and_more'),
+        ('calendar_app', '0002_calendarsettings_push_sent'),
+    ]
+
+    operations = []
